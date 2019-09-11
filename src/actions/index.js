60ca02f5c97ed258212329f31f63f43e2ba3d842
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-11 14:51:51 -0700

    Added remove and buy actions in index.js/actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,18 @@
-const removeFeature = item => {
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
+export const removeFeature = item => {
     // dispatch an action here to remove an item
+    return {
+        type: 'REMOVE_FEATURE',
+        payload: item
+    };
   };
 
-  const buyItem = item => {
+
+export const BUY_ITEM = 'BUY_ITEM';  
+export const buyItem = item => {
     // dipsatch an action here to add an item
+    return {
+        type: 'BUY_ITEM',
+        payload: item
+    };
   };
